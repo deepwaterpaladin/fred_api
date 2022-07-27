5a51754c44daf05c2bf05d6feb52033a4941b531
deepwaterpaladin <rielly.young11@gmail.com>
2022-07-27 07:41:59 -0400

    changes

--- bonds.py
+++ bonds.py
@@ -9,13 +9,21 @@ class CorporateBonds():
     def AAA(self, timeframe = str, observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
         '''Moody's Seasoned Aaa Corporate Bond Yield (Weekly or Monthly)'''
         if timeframe == 'Weekly':
-            self.data = fred.get_series('WAAA', observation_start='01/05/1962', observation_end=observation_end)
+            self.data = fred.get_series('WAAA', observation_start='01/05/1962', observation_end=observation_end) # Weekly series
         elif timeframe == 'Daily':
-            self.data = fred.get_series('DAAA', observation_start = '01/01/1985', observation_end = observation_end)
+            self.data = fred.get_series('DAAA', observation_start = '01/01/1985', observation_end = observation_end) # Daily series
         else:
-            self.data = fred.get_series('AAA', observation_start='01/01/1919', observation_end=observation_end)
+            self.data = fred.get_series('AAA', observation_start='01/01/1919', observation_end=observation_end) # Monthly series
         return pd.DataFrame(self.data,columns=['Percent'])
-    
+    def BAA(self, timeframe = str, observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Moody's Seasoned Baa Corporate Bond Yield'''
+        if timeframe == 'Weekly':
+            self.data = fred.get_series('WBAA', observation_start='01/05/1962', observation_end=observation_end) # Weekly series
+        elif timeframe == 'Daily':
+            self.data = fred.get_series('DBAA', observation_start = '01/01/1985', observation_end = observation_end) # Daily series
+        else:
+            self.data = fred.get_series('BAA', observation_start='01/01/1919', observation_end=observation_end) # Monthly series
+        return pd.DataFrame(self.data,columns=['Percent'])
     def AAA_to_10_Year(self, daily = True, observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
         '''Moody's Seasoned AAA Corporate Bond Yield Relative to Yield on 10-Year Treasury Constant Maturity (Daily or Monthly)'''
         if daily == True:
@@ -23,3 +31,22 @@ class CorporateBonds():
         else:
             self.data = fred.get_series('AAA10YM', observation_start='04/01/1953', observation_end=observation_end)
         return pd.DataFrame(self.data,columns=['Percent'])
+    def BAA_to_10_Year(self, daily = True, observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Moody's Seasoned Baa Corporate Bond Yield Relative to Yield on 10-Year Treasury Constant Maturity (Daily or Monthly)'''
+        if daily == True:
+            self.data = fred.get_series('BAA10Y', observation_start='01/03/1986', observation_end=observation_end)
+        else:
+            self.data = fred.get_series('BAA10YM', observation_start='04/01/1953', observation_end=observation_end)
+        return pd.DataFrame(self.data,columns=['Percent'])
+    def AAA_minus_FFR(self, daily = True, observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Moody's Seasoned Aaa Corporate Bond Minus Federal Funds Rate (Daily or Monthly)'''
+        if daily == True:
+            self.data = fred.get_series('AAAFF', observation_start='01/03/1983', observation_end=observation_end)
+        else:
+            self.data = fred.get_series('AAAFFM', observation_start='07/01/1954', observation_end=observation_end)
+        return pd.DataFrame(self.data,columns=['Percent'])
+
+
+        
+
+
--- commodities.py
+++ commodities.py
@@ -52,6 +52,13 @@ class Commodity():
     def fish(self, observation_start='01/01/1985', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
         self.data = fred.get_series('PSALMUSDA', observation_start = observation_start, observation_end=observation_end)
         return pd.DataFrame(self.data,columns=['Price'])
+    def shellfish(self, adjusted=False, observation_start='01/01/1985', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Producer Price Index by Commodity: Processed Foods and Feeds: Unprocessed Shellfish'''
+        if adjusted == True:
+            self.data = fred.get_series('WPS022305', observation_start = observation_start, observation_end=observation_end)
+        else:
+            self.data = fred.get_series('WPU022305', observation_start = observation_start, observation_end=observation_end)
+        return pd.DataFrame(self.data,columns=['Index December 1991 = 100'])
     def nickel(self, observation_start='01/01/1985', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
         self.data = fred.get_series('PNICKUSDM', observation_start = observation_start, observation_end=observation_end)
         return pd.DataFrame(self.data,columns=['Price'])
@@ -83,6 +90,48 @@ class Commodity():
         self.data = fred.get_series('PLAMBUSDM', observation_start = observation_start, observation_end=observation_end)
         return pd.DataFrame(self.data,columns=['Price'])
 
-
+class NaturalGas():
+    def __init__(self) -> None:
+        pass
+    def spot_price(self, format='daily', observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Henry Hub Natural Gas Spot Price'''
+        if format == 'monthly':
+            self.data = fred.get_series('MHHNGSP', observation_start = observation_start, observation_end=observation_end)
+        elif format == 'weekly':
+            self.data = fred.get_series('WHHNGSP', observation_start = observation_start, observation_end=observation_end)
+        elif format == 'annual' or format == 'yearly':
+            self.data = fred.get_series('AHHNGSP', observation_start = observation_start, observation_end=observation_end)
+        else:
+            self.data = fred.get_series('DHHNGSP', observation_start = observation_start, observation_end=observation_end)
+        return pd.DataFrame(self.data, columns=['Dollars per Million BTUs'])
+    def ppi(self, observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Producer Price Index by Commodity: Fuels and Related Products and Power: Natural Gas '''
+        self.data = fred.get_series('WPU0531', observation_start = observation_start, observation_end=observation_end)
+        return pd.DataFrame(self.data, columns=['Index 1982=100'])
+    def ppi_to_consumers(self, observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Producer Price Index by Industry: Natural Gas Distribution: Natural Gas to Ultimate Consumers (Monthly)'''
+        self.data = fred.get_series('PCU22121022121011', observation_start = observation_start, observation_end=observation_end)
+        return pd.DataFrame(self.data, columns=['Index December 1990=100'])
+    def consumption(self, adjusted=True, observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Natural Gas Consumption'''
+        if adjusted==True:
+            self.data = fred.get_series('NATURALGASD11', observation_start = observation_start, observation_end = observation_end) # Seasonally Adjusted
+        else:
+            self.data = fred.get_series('NATURALGAS', observation_start = observation_start, observation_end = observation_end) # Non Seasonally Adjusted
+        return pd.DataFrame(self.data, columns=['Billion Cubic Feet'])
+    def LNG_composite_price(self, format='monthly', observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''U.S. Natural Gas Liquid Composite Price'''
+        if format == 'annually' or format == 'yearly':
+            self.data = fred.get_series('ANGLCP', observation_start = observation_start, observation_end = observation_end)
+        else:
+            self.data = fred.get_series('MNGLCP', observation_start = observation_start, observation_end = observation_end)
+        return pd.DataFrame(self.data, columns=['Dollars per Million BTUs'])
+    def capacity_utilization(self, format='monthly', observation_start='01/01/2000', observation_end=date.today().strftime("%m/%d/%Y"))->pd.DataFrame:
+        '''Capacity Utilization: Utilities: Natural Gas Distribution (NAICS = 2212)'''
+        if format == 'quarterly':
+            self.data = fred.get_series('CAPUTLG2212SQ', observation_start = observation_start, observation_end = observation_end)
+        else:
+            self.data = fred.get_series('CAPUTLG2212S', observation_start = observation_start, observation_end = observation_end)
+        return pd.DataFrame(self.data, columns=['Percent of Capacity'])
 
 
